add2b0c777a96a6e1080f66b12be4fcb30634ac5
권민수 <kms37480@naver.com>
2023-07-09 14:54:58 +0000

    so hard..

--- baekjoon/stack/4949.cpp
+++ baekjoon/stack/4949.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 int main(){
 	char sentence;
-	stack<char>stack;
-	sentence=getchar();
+	stack<char>stack1;
+	stack<char>stack2;
+	int k;
 		while(1){
 			sentence=getchar();
-			if(sentence=='('||sentence=='[')
+			
+			if(sentence=='(')
 			{
-				stack.push(sentence);
+				stack1.push(sentence);
+				k=1;
+				
 			}
-			else if(sentence==')'||sentence==']')
+			else if(sentence==')')
 			{
-				if(stack.empty()==true)
+				k=0;
+				if(stack1.empty()==true)
 				{
 					while(!sentence=='.')
 					{
@@ -23,24 +28,73 @@ int main(){
 					cout<<"no"<<'\n';
 					break;
 				}
-				stack.pop();
+				
+				stack1.pop();
+			}
+			else if(k==1&&sentence==']')
+			{
+					while(!sentence=='.')
+					{
+						sentence=getchar();
+						
+					}	
+					cout<<"no"<<'\n';
+					break;
+			}
+			else if(sentence=='[')
+			{
+				stack2.push(sentence);
+				k=2;
+			}
+			else if(sentence==']')
+			{
+				k=0;
+				if(stack2.empty()==true)
+				{
+					while(!sentence=='.')
+					{
+						sentence=getchar();
+						
+					}	
+					cout<<"no"<<'\n';
+					break;
+				}
+				
+				stack2.pop();
+			}
+			else if(k==2&&sentence==')')
+			{
+				
+					while(!sentence=='.')
+					{
+						sentence=getchar();
+						
+					}	
+					cout<<"no"<<'\n';
+					break;
 			}
 			else if(sentence=='.')
 			{
-				if(stack.empty()==true)
+				if(stack1.empty()&&stack2.empty()==true)
 				{
 					cout<<"yes"<<'\n';
 				}
 				else
 				{
-					while(!stack.empty()==true)
+					while(!stack1.empty()==true)
 					{
-						stack.pop();
+						stack1.pop();
+					}
+					while(!stack2.empty()==true)
+					{
+						stack2.pop();
 					}
 					cout<<"no"<<'\n';
 				}
 				break;
 			}
+			
+		
 		}
 		
 	}
